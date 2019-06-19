8fc04d39bcc29a701dd80af917bc4bac1fec4026
wh01Samyak <sb472@snu.edu.in>
2019-06-19 21:06:08 +0530

    Comparision module corrected.

--- Python/text_compare.py
+++ Python/text_compare.py
@@ -3,7 +3,7 @@ import diff_match_patch as dmp_module
 def diff(text1,text2):
 	dmp = dmp_module.diff_match_patch()
 	diff = dmp.diff_main(text1, text2)
-	errors = dmp.diff_cleanupSemantic(diff)
-	return errors
+	dmp.diff_cleanupSemantic(diff)
+	return diff
 
 
